604306a3901b743ddbb671d64344e248a2926aea
soumildatta <soumildatta@gmail.com>
2023-10-27 12:31:24 -0500

    Read two files

--- main.cpp
+++ main.cpp
@@ -8,15 +8,27 @@ using std::endl;
 using std::ifstream;
 
 int main(int argc, char *argv[]) {
-    char *filename = argv[1];
+    char *filename1 = argv[1];
+    char *filename2 = argv[2];
 
     // Read from a file:
-    ifstream infile(filename);
+    ifstream infile1(filename1);
 
     char c;
-    while (infile.get(c)) {
+    while (infile1.get(c)) {
         cout << c;
     }
+
+    // Read from second file 
+    ifstream infile2(filename2);
+
+    while (infile2.get(c)) {
+        cout << c;
+    }
+
+    // close the ifstreams
+    infile1.close();
+    infile2.close();
 
     return 0;
 }
