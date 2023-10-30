3e45cdb950f93d30fb9108164ed27dd3882509ef
soumildatta <soumildatta@gmail.com>
2023-10-30 11:57:06 -0500

    Display diff

--- main.cpp
+++ main.cpp
@@ -16,6 +16,8 @@ using std::memset;
 #include <string>
 using std::string;
 
+#include <stack>
+using std::stack;
 
 #include <algorithm>
 using std::max;
@@ -65,6 +67,41 @@ void diff(string s1, string s2) {
     vector<vector<int>> dp = LCS(lines1, lines2);
 
     // Find and display the differences
+    stack<string> lineStack;
+
+    int i = lines1.size(), j = lines2.size();
+    while (i > 0 && j > 0) {
+        // If the lines are the same, move on
+        if (lines1[i-1] == lines2[j-1]) {
+            --i;
+            --j;
+        } else if (dp[i-1][j] > dp[i][j-1]) { // If the line in file 1 is longer than the line in file 2
+
+            lineStack.emplace("< " + lines1[i-1]);
+            --i;
+        } else { // If the line in file 2 is longer than the line in file 1
+            lineStack.emplace("> " + lines2[j-1]);
+            --j;
+        }
+    }
+
+    // If there are any remaining lines in file 1, add them to the stack
+    while (i > 0) {
+        lineStack.emplace("< " + lines1[i-1]);
+        --i;
+    }
+
+    // If there are any remaining lines in file 2, add them to the stack
+    while (j > 0) {
+        lineStack.emplace("> " + lines2[j-1]);
+        --j;
+    }
+
+    // Print the stack
+    while (!lineStack.empty()) {
+        cout << lineStack.top() << endl;
+        lineStack.pop();
+    }
 }
 
 
@@ -106,8 +143,7 @@ int main(int argc, char *argv[]) {
 
     // string s1 = "AGGTAB";
     // string s2 = "GXTXAYB";
-    cout << s1 << endl << s2 << endl;
-    cout << "Length of LCS is " << lcs(s1, s2) << endl;
+    diff(s1, s2);
 
     return 0;
 }
